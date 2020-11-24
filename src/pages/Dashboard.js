1698619868c013ee036f55a6c6b4b6c39b565b96
Eric1995 <nickchoi1995@gmail.com>
2020-11-25 00:33:01 +0800

    Updated AutoComplete

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,11 +1,24 @@
+import { useState } from 'react';
+import { AutoComplete } from '../components/AutoComplete';
 import { Button } from '../components/Button';
 
 const Dashboard = () => {
-
+    let initValue = {
+        title: '',
+        null: null
+    }
+    const [value, setValue] = useState(initValue)
     return (
         <div>
             <p>Dashboard</p>
             <Button />
+            <AutoComplete
+                initValue={initValue}
+                value={value}
+                setValue={setValue}
+                label="Select Film" />
+            <p>{value.title}</p>
+            <p>{value.year}</p>
         </div>
     )
 }
