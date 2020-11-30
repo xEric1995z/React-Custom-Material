bbec5923d736b8acaa0cd18c7e1d24a8f500d0b4
Eric1995 <nickchoi1995@gmail.com>
2020-11-30 16:51:39 +0800

    Updated travis ci

---
+++ src/components/FormInput.js
@@ -0,0 +1,11 @@
+import { TextField } from '@material-ui/core';
+
+export const FormInput = () => {
+
+    return (
+        <TextField
+          label="Helper text"
+          defaultValue="Default Value"
+          helperText="Some important text" />
+    )
+}
--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,24 +1,31 @@
 import { useState } from 'react';
+import { FormInput } from '../components/FormInput'
 import { AutoComplete } from '../components/AutoComplete';
 import { Button } from '../components/Button';
 
 const Dashboard = () => {
     let initValue = {
         title: '',
-        null: null
+        year: null
     }
-    const [value, setValue] = useState(initValue)
+    const [value, setValue] = useState(initValue);
+    const [str, setStr] = useState('');
+    
+    const handleChange = (event) => {
+        
+    }
+
     return (
         <div>
             <p>Dashboard</p>
             <Button />
             <AutoComplete
                 initValue={initValue}
-                value={value}
                 setValue={setValue}
                 label="Select Film" />
             <p>{value.title}</p>
             <p>{value.year}</p>
+            <FormInput />
         </div>
     )
 }
